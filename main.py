227c87fee0d8347163e9fa5ad47e5fb9f54c6052
haruki-yamane <69576348+haruki-yamane@users.noreply.github.com>
2023-07-07 10:07:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -57,5 +57,5 @@ def handle_location(event):
     )
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 10000))
+    port = int(os.getenv("PORT", 80))
     app.run(debug=True, host="0.0.0.0", port=port)
