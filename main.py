35fa19572bea684f6aecf51121c735db3c1b735e
haruki-yamane <69576348+haruki-yamane@users.noreply.github.com>
2023-07-07 13:19:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -50,8 +50,8 @@ def handle_message(event):
 
 @handler.add(MessageEvent, message=LocationMessage)
 def handle_location(event):
-    #text = event.message.address
-    result = "hogehohe"
+    text = event.message.address
+    result = text
     #result = sc.get_weather_from_location(text)
     line_bot_api.reply_message(
         event.reply_token,
